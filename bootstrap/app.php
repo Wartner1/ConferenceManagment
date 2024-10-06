ad7151f1546aeb26e4f191708c139003a613db52
Wartner1 <deivid.sokol@gmail.com>
2024-10-06 23:54:34 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    //testas
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
